6076ed9fa268ae025afc8f3f473c15956afa7d32
Kent Sutherland <git@ksuther.com>
2013-12-01 22:32:36 -0500

    Switch from using WaxSim to ios-sim since it is kept up to date more frequently.

--- make_screenshots.py
+++ make_screenshots.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-#Usage: python make_screenshots.py /path/to/screenshot/output
-#This script will create the output directory if necessary.
-#Set
+# Usage: python make_screenshots.py /path/to/screenshot/output
+# This script will create the output directory if necessary.
 
 import os
 import sys
@@ -10,10 +9,10 @@ import glob
 import argparse
 import json
 
-def compile_waxsim():
+def compile_iossim():
     previous_dir = os.getcwd()
-    os.chdir(os.path.join(os.path.realpath(os.path.split(__file__)[0]), 'Contributed', 'WaxSim'))
-    subprocess.call(['xcodebuild', '-target', 'WaxSim', '-configuration', 'Release', 'clean', 'build', 'SYMROOT=build'], stdout=open('/dev/null', 'w'))
+    os.chdir(os.path.join(os.path.realpath(os.path.split(__file__)[0]), 'Contributed', 'ios-sim'))
+    subprocess.call(['xcodebuild', '-scheme', 'ios-sim', '-configuration', 'Release', 'clean', 'build', 'SYMROOT=build'], stdout=open('/dev/null', 'w'))
     os.chdir(previous_dir)
 
 def compile_app():
@@ -27,17 +26,21 @@ def compile_app():
 def quit_simulator():
     subprocess.call(['killall', 'iPhone Simulator'])
 
-def set_device(device):
-    subprocess.call(['defaults', 'write', 'com.apple.iphonesimulator', 'SimulateDevice', device])
+def iossim(app_path, args, device):
+    iossim_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'Contributed', 'ios-sim', 'build', 'Release', 'ios-sim')
+    subprocess_args = [iossim_path]
 
-def waxsim(app_path, args, device):
-    waxsim_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'Contributed', 'WaxSim', 'build', 'Release', 'waxsim')
-    subprocess_args = [waxsim_path]
-    
+    subprocess_args += ['launch', app_path]
+
+    # ios-sim does the default setting itself, so convert the device name into arguments that ios-sim expects
     if 'iPad' in device:
-        subprocess_args += ['-f', 'ipad']  
-    
-    subprocess_args += [app_path]    
+        subprocess_args += ['--family', 'ipad']
+    elif 'iPhone Retina (3.5-inch)' in device:
+        subprocess_args += ['--family', 'iphone', '--retina']
+    elif 'iPhone Retina (4-inch)' in device:
+        subprocess_args += ['--family', 'iphone', '--retina', '--tall']
+
+    subprocess_args += ['--args']
     subprocess_args += args
     
     subprocess.call(subprocess_args)
@@ -71,7 +74,7 @@ if __name__ == '__main__':
     if os.path.isabs(options['project_path']):
         project_path = options['project_path']
     else:
-        #project_path is relative to the parent directory of config_path
+        # project_path is relative to the parent directory of config_path
         project_path = os.path.realpath(os.path.join(os.path.dirname(config_path), options['project_path']))
     
     app_path = os.path.join(project_path, 'build', options['build_config'] + '-iphonesimulator', options['app_name'])
@@ -79,16 +82,15 @@ if __name__ == '__main__':
     print 'Building with ' + options['build_config'] + ' configuration...'
     compile_app()
     
-    print 'Building WaxSim...'
-    compile_waxsim()
     
-    #create destination directory
+    print 'Building ios-sim...'
+    compile_iossim()
+    # create destination directory
     if not os.path.exists(options['destination_path']):
         os.makedirs(options['destination_path'])
         
     for device in options['devices']:
         quit_simulator()
-        set_device(device)
         
         for language in options['languages']:
             language_path = os.path.join(options['destination_path'], language)
@@ -97,6 +99,6 @@ if __name__ == '__main__':
             
             print 'Creating screenshots for {} using {}...'.format(language, device)
             
-            waxsim(app_path, ['-AppleLanguages', '({})'.format(language), '-AppleLocale', language, language_path], device)
+            iossim(app_path, ['-AppleLanguages', '({})'.format(language), '-AppleLocale', language, language_path], device)
 
     quit_simulator()
