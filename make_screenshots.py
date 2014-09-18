0da55744670468a99ed0f731829122883c501007
Kent Sutherland <git@ksuther.com>
2014-09-18 13:05:07 -0400

    Allow specifying a scheme using scheme_name. Can use scheme_name or target_name, but scheme_name takes precedence. Pass derivedDataPath to xcodebuild to ensure a unique derived data directory is used for the build.

--- make_screenshots.py
+++ make_screenshots.py
@@ -13,7 +13,7 @@ import shutil
 def compile_iossim():
     previous_dir = os.getcwd()
     os.chdir(os.path.join(os.path.realpath(os.path.split(__file__)[0]), 'Contributed', 'ios-sim'))
-    subprocess.call(['xcodebuild', '-scheme', 'ios-sim', '-configuration', 'Release', 'clean', 'build', 'SYMROOT=build'], stdout=open('/dev/null', 'w'))
+    subprocess.call(['xcodebuild', '-scheme', 'ios-sim', '-configuration', 'Release', '-derivedDataPath', 'build', 'clean', 'build', 'SYMROOT=build'], stdout=open('/dev/null', 'w'))
     os.chdir(previous_dir)
 
 def compile_app():
@@ -21,7 +21,11 @@ def compile_app():
     os.chdir(project_path)
     
     # Force the simulator build to use 32-bit, otherwise UIGetScreenImage doesn't exist
-    subprocess.call(['xcodebuild', '-target', options['target_name'], '-configuration', options['build_config'], '-sdk', 'iphonesimulator', 'clean', 'build', 'SYMROOT=build', 'ARCHS=i386', 'ONLY_ACTIVE_ARCH=NO'], stdout=open('/dev/null', 'w'))
+    if 'scheme_name' in options:
+        subprocess.call(['xcodebuild', '-scheme', options['scheme_name'], '-configuration', options['build_config'], '-sdk', 'iphonesimulator', '-derivedDataPath', 'build', 'clean', 'build', 'ARCHS=i386', 'ONLY_ACTIVE_ARCH=NO'], stdout=open('/dev/null', 'w'))
+    else:
+        subprocess.call(['xcodebuild', '-target', options['target_name'], '-configuration', options['build_config'], '-sdk', 'iphonesimulator', 'clean', 'build', 'SYMROOT=build', 'ARCHS=i386', 'ONLY_ACTIVE_ARCH=NO'], stdout=open('/dev/null', 'w'))
+
     os.chdir(previous_dir)
 
 def quit_simulator():
@@ -73,14 +77,17 @@ if __name__ == '__main__':
         # project_path is relative to the parent directory of config_path
         project_path = os.path.realpath(os.path.join(os.path.dirname(config_path), options['project_path']))
     
-    app_path = os.path.join(project_path, 'build', options['build_config'] + '-iphonesimulator', options['app_name'])
+    if 'scheme_name' in options:
+        app_path = os.path.join(project_path, 'build', 'Build', 'Products', options['build_config'] + '-iphonesimulator', options['app_name'])
+    else:
+        app_path = os.path.join(project_path, 'build', options['build_config'] + '-iphonesimulator', options['app_name'])
     
     print 'Building with ' + options['build_config'] + ' configuration...'
     compile_app()
     
-    
     print 'Building ios-sim...'
     compile_iossim()
+    
     # create destination directory
     if not os.path.exists(options['destination_path']):
         os.makedirs(options['destination_path'])
