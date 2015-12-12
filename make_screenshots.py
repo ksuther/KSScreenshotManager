490280128641a55704a04763cb15cc172dcc2023
Stephane Peter <megastep@megastep.org>
2015-12-12 00:41:20 -0800

    Added command line options to override device and language, and default to loading config.json unless specified.

--- make_screenshots.py
+++ make_screenshots.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Usage: python make_screenshots.py /path/to/screenshot/output
 # This script will create the output directory if necessary.
 
@@ -22,19 +22,18 @@ def compile_app():
         arguments = ['xcrun', 'xcodebuild', '-scheme', options['scheme_name'], '-configuration', options['build_config'], '-destination', options['build_destination'], '-derivedDataPath', 'build', 'clean', 'build']
     else:
         arguments = ['xcrun', 'xcodebuild', '-target', options['target_name'], '-configuration', options['build_config'], '-destination', options['build_destination'], 'clean', 'build', 'SYMROOT=build']
-
+    
     if 'skip_clean' in options and options['skip_clean']:
         # Don't clean the build before building and running
         arguments.remove('clean')
-
-    print "Build command: %s" % arguments
+    
+    print "Recompiling app: %s" % arguments
     subprocess.call(arguments, stdout=open('/dev/null', 'w'))
-
     os.chdir(previous_dir)
 
 def quit_simulator():
     subprocess.call(['killall', 'Simulator'])
-    
+
 def reset_simulator():
     shutil.rmtree(os.path.expanduser('~/Library/Application Support/iPhone Simulator'), ignore_errors=True)
 
@@ -47,7 +46,7 @@ def simctl(device, app, args, output_path):
     subprocess_args = ['xcrun', 'simctl', 'launch', device, app]
     subprocess_args += args
     subprocess_args += [output_path]
-    
+
     status = output_path + "/.screenshots.tmp"
     if os.path.isfile(status):
         os.remove(status) # Start from clean slate
@@ -67,7 +66,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build iOS screenshots.')
     parser.add_argument('--skip-clean', '-s', action='store_true', help='skip clean when calling xcodebuild')
     parser.add_argument('--path', '-p', dest='destination', help='destination path for screenshots (overrides config)')
-    parser.add_argument('config', help='path to JSON config file')
+    parser.add_argument('--device', '-d', dest='device_name', help='Run on a single device, ignoring config')
+    parser.add_argument('--lang', '-l', dest='language', help='Run for a specific locale, ignoring config')
+    parser.add_argument('config', help='path to JSON config file', default='config.json', nargs='?')
 
     args = parser.parse_args()
     config_path = args.config
@@ -91,6 +92,12 @@ if __name__ == '__main__':
     if args.skip_clean:
         options['skip_clean'] = True
 
+    if args.device_name:
+        options['device_names'] = [args.device_name]
+
+    if args.language:
+        options['languages'] = [args.language]
+
     ###
     
     if os.path.isabs(options['project_path']):
@@ -98,7 +105,9 @@ if __name__ == '__main__':
     else:
         # project_path is relative to the parent directory of config_path
         project_path = os.path.realpath(os.path.join(os.path.dirname(config_path), options['project_path']))
-    
+
+    print 'Project path is ' + project_path
+
     if 'scheme_name' in options:
         app_path = os.path.join(project_path, 'build', 'Build', 'Products', options['build_config'] + '-iphonesimulator', options['app_name'])
     else:
@@ -107,7 +116,7 @@ if __name__ == '__main__':
     if 'build_destination' not in options:
         # no destination was specified, assume we're building for an iPhone 4s so that we end up with a 32-bit binary that will run everywhere
         options['build_destination'] = 'name=iPhone 4s'
-    
+
     print 'Building with ' + options['build_config'] + ' configuration...'
     compile_app()
     
@@ -128,7 +137,7 @@ if __name__ == '__main__':
             if 'reset_between_runs' in options and options['reset_between_runs']:
                 quit_simulator()
                 reset_simulator()
-            
+
             simctl(device, options['app_id'], ['-AppleLanguages', '({})'.format(language), '-AppleLocale', language], language_path)
 
     quit_simulator()
